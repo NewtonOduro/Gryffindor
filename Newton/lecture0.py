0a11e2e4841d08a36f249901fb809494dd8a0f71
ISAAC ADU NTI <odonti40@gmail.com>
2026-02-17 13:32:04 +0000

    odonti: this is my code

--- Newton/lecture0.py
+++
@@ -1,9 +0,0 @@
-print("heelo world")
-
-name = input("What is your name:")
-print(f"Hello, {name} "+" 12 ")
-
-# removing spaces with the strip() function
-name = input("Enter your name:")
-name = name.strip().capitalize()
-print("Hello")
